35842d829578852c562df89c3a2172b2ccb62ab7
Achraf <a.ahsaine0203@uca.ac.ma>
2026-05-10 16:55:20 +0100

    init: cv project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         
         <div className="social-links">
           <a href="mailto:achrafahsaine06@gmail.com" className="social-icon" title="Email"><i className="fa-solid fa-envelope"></i></a>
-          <a href="https://linkedin.com/in/achraf-ahsaine" target="_blank" rel="noreferrer" className="social-icon" title="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
+          <a href="https://www.linkedin.com/in/achraf-ahsaine-a21a083ba" target="_blank" rel="noreferrer" className="social-icon" title="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
           <a href="https://github.com/aahsaine0203-lab" target="_blank" rel="noreferrer" className="social-icon" title="GitHub"><i className="fa-brands fa-github"></i></a>
           <a href="tel:+212627225925" className="social-icon" title="Phone: +212 627225925"><i className="fa-solid fa-phone"></i></a>
         </div>
